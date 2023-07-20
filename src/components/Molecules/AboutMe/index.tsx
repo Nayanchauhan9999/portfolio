d1a346a638eaef043deb25cc0ac6f9b403ac619d
Nayan Chauhan <nayanchauhan9999@gmail.com>
2023-07-20 16:34:29 +0530

    fix about me section for mobile error : when toggle dark mode career objective move aside due to margin

--- src/components/Molecules/AboutMe/index.tsx
+++ src/components/Molecules/AboutMe/index.tsx
@@ -36,7 +36,7 @@ const AboutMe = ({}: AboutMeProps) => {
           >
             A Web Developer
           </Heading>
-          <CareerObjective margin="small" textalign="justify">
+          <CareerObjective margin="none" textalign="justify">
             I am a self-taught programmer who believes in lifelong learning. I
             am very passionate about being a web developer. Being a web
             developer allows me to blend my creativity with my technical skills,
