8214fc0648894e23f7fa1b04be6915994aec7708
nayanchauhan9999 <nayanchauhan9999@gmail.com>
2024-06-02 17:38:53 +0530

    update packages

--- src/components/__Shared/Container/index.tsx
+++ src/components/__Shared/Container/index.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React from "react";
-import { Container } from "./container.types";
+import type { Container } from "./container.types";
 import { StyledContainer } from "./container.style";
 
 const Container = ({ children }: Container) => {
